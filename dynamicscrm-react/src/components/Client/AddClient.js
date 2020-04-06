3128be122616c5a572ecb126834af8d3ab082c60
declanmurphy1 <declanmurphy1117@gmail.com>
2020-04-06 15:54:44 -0400

    Update client form errors

--- dynamicscrm-react/src/components/Client/AddClient.js
+++ dynamicscrm-react/src/components/Client/AddClient.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { createClient } from "../../actions/clientActions";
+import classnames from "classnames";
 
 class AddClient extends Component {
   constructor() {
@@ -66,94 +67,137 @@ class AddClient extends Component {
                   <div className="form-group">
                     <input
                       type="text"
-                      className="form-control form-control-lg "
+                      className={classnames("form-control form-control-lg",{
+                        "is-invalid":errors.clientName
+                      })}
                       placeholder="Client Name"
                       name="clientName"
                       value={this.state.clientName}
                       onChange={this.onChange}
                     />
-                    <p>{errors.clientName}</p>
+                    {errors.clientName && (
+                      <div className="invalid-feedback">{errors.clientName}</div>
+                    )}
                   </div>
                   <div className="form-group">
                     <input
                       type="text"
-                      className="form-control form-control-lg"
+                      className={classnames("form-control form-control-lg",{
+                        "is-invalid":errors.clientIdentifier
+                      })}
                       placeholder="Unique Client ID"
                       name="clientIdentifier"
                       value={this.state.clientIdentifier}
                       onChange={this.onChange}
                     />
-                    <p>{errors.clientIdentifier}</p>
+                    {errors.clientIdentifier && (
+                      <div className="invalid-feedback">{errors.clientIdentifier}</div>
+                    )}
                   </div>
                   <div className="form-group">
                     <input
                       type="text"
-                      className="form-control form-control-lg"
+                      className={classnames("form-control form-control-lg",{
+                        "is-invalid":errors.owner
+                      })}
                       placeholder="Client Owner"
                       name="owner"
                       value={this.state.owner}
                       onChange={this.onChange}
                     />
+                    {errors.owner && (
+                      <div className="invalid-feedback">{errors.owner}</div>
+                    )}
                   </div>
                   <div className="form-group">
                     <input
                       type="text"
-                      className="form-control form-control-lg"
+                      className={classnames("form-control form-control-lg",{
+                        "is-invalid":errors.industry
+                      })}
                       placeholder="Industry"
                       name="industry"
                       value={this.state.industry}
                       onChange={this.onChange}
                     />
+                    {errors.industry && (
+                      <div className="invalid-feedback">{errors.industry}</div>
+                    )}
                   </div>
                   <div className="form-group">
                     <textarea
-                      className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg",{
+                      "is-invalid":errors.description
+                    })}
                       placeholder="Client Description"
                       name="description"
                       value={this.state.description}
                       onChange={this.onChange}
                     ></textarea>
+                    {errors.description && (
+                      <div className="invalid-feedback">{errors.description}</div>
+                    )}
                   </div>
                   <h6>Address</h6>
                   <div className="form-group">
                     <input
                       type="text"
-                      className="form-control form-control-lg"
+                      className={classnames("form-control form-control-lg",{
+                        "is-invalid":errors.addressLine1
+                      })}
                       placeholder="Line 1"
                       name="addressLine1"
                       value={this.state.addressLine1}
                       onChange={this.onChange}
                     />
+                    {errors.addressLine1 && (
+                      <div className="invalid-feedback">{errors.addressLine1}</div>
+                    )}
                   </div>
                   <div className="form-group">
                     <input
                       type="text"
-                      className="form-control form-control-lg"
+                      className={classnames("form-control form-control-lg",{
+                        "is-invalid":errors.addressLine2
+                      })}
                       placeholder="Line 2"
                       name="addressLine2"
                       value={this.state.addressLine2}
                       onChange={this.onChange}
                     />
+                    {errors.addressLine2 && (
+                      <div className="invalid-feedback">{errors.addressLine2}</div>
+                    )}
                   </div>
                   <div className="form-group">
                     <input
                       type="text"
-                      className="form-control form-control-lg"
+                      className={classnames("form-control form-control-lg",{
+                        "is-invalid":errors.county
+                      })}
                       placeholder="County"
                       name="county"
                       value={this.state.county}
                       onChange={this.onChange}
                     />
+                    {errors.county && (
+                      <div className="invalid-feedback">{errors.county}</div>
+                    )}
                   </div>
                   <div className="form-group">
                     <input
                       type="text"
-                      className="form-control form-control-lg"
+                      className={classnames("form-control form-control-lg",{
+                        "is-invalid":errors.country
+                      })}
                       placeholder="Country"
                       name="country"
                       value={this.state.country}
                       onChange={this.onChange}
                     />
+                    {errors.country && (
+                      <div className="invalid-feedback">{errors.country}</div>
+                    )}
                   </div>
 
                   <input
